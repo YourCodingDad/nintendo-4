7ca079ccac53ebf6dc77172d4003d44e70c089fa
Marie <malestro56@gmail.com>
2026-03-04 16:40:07 +0100

    ajout classe adresse

---
+++ NintendoProject/src/main/java/nintendo/model/Boutique.java
@@ -0,0 +1,28 @@
+package nintendo.model;
+
+public class Boutique {
+private String nom;
+private String adresse;
+public Boutique(String nom, String adresse) {
+	super();
+	this.nom = nom;
+	this.adresse = adresse;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getAdresse() {
+	return adresse;
+}
+public void setAdresse(String adresse) {
+	this.adresse = adresse;
+}
+@Override
+public String toString() {
+	return "Boutique [nom=" + nom + ", adresse=" + adresse + "]";
+}
+
+}
