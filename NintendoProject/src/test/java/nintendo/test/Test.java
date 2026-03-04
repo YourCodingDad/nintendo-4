65ba4d8bf1e14a3d1bbc547aff13fc057c0b3cf6
YourCodingDad <fabian.labonne@outlook.fr>
2026-03-04 17:35:40 +0100

    correction finale

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nintendo.model.Achat;
-import nintendo.model.Client;
 import nintendo.model.Boutique;
+import nintendo.model.Client;
 import nintendo.model.Console;
 import nintendo.model.Jeu;
+import nintendo.model.Salon;
 
 public class Test {
 
@@ -18,21 +19,23 @@ public class Test {
 		List<Achat> l1 = new ArrayList();
 		List<Achat> l2 = new ArrayList();
 		
-		Console xbox = new Console("Xbox");
-		Console playStation = new Console("playStation");
-		Console pc = new Console("pc");
 		
-		Jeu StreetFighter1 = new Jeu("Street Fighter 1",playStation);
-		Jeu yakuza0 = new Jeu("Yakuza0", pc);
-		Jeu theWitcher3 = new Jeu("The Witcher 3",xbox);
-		Jeu skyrimRemastered = new Jeu("Skyrim Remastered", xbox);
-		Jeu womanSimulator = new Jeu("Woman Simulator", pc);
+		Boutique jeux_video = new Boutique("Video Futur", "7 rue de la clé 59000 Lille");
+		
+		Console xbox = new Salon("Xbox");
+		Console playStation = new Salon("playStation");
+		Console pc = new Salon("pc");
+		
+		Jeu StreetFighter1 = new Jeu("Street Fighter 1",playStation,jeux_video);
+		Jeu yakuza0 = new Jeu("Yakuza0", pc,jeux_video);
+		Jeu theWitcher3 = new Jeu("The Witcher 3",xbox,jeux_video);
+		Jeu skyrimRemastered = new Jeu("Skyrim Remastered", xbox,jeux_video);
+		Jeu womanSimulator = new Jeu("Woman Simulator", pc,jeux_video);
 		
 		l1.add(new Achat(StreetFighter1,LocalDate.now(),250));
 		l2.add(new Achat(skyrimRemastered,LocalDate.now(),10));
 		Client c1 = new Client("Doe","Jane", l1);
 		Client c2 = new Client("Doe","John",l2);
-		Boutique jeux_video = new Boutique("Video Futur", "7 rue de la clé 59000 Lille");
 		
 	
 		
